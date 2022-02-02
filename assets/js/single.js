c787aca619991707fd082945ed696baec8c122dc
alen <alen.dunn1@gmail.com>
2022-02-02 14:18:48 -0500

    issue #2 resolved

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,22 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if(repoName){
+        // display repo name on the page
+    getRepoIssues(repoName);
+    repoNameEl.textContent = repoName;
+    }else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+}
+
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
     fetch(apiUrl).then(function(response){
@@ -13,11 +30,12 @@ var getRepoIssues = function(repo) {
                 }
             });
         }else{
-            alert("There was a problem with your request!");
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
-getRepoIssues("facebook/react");
+
 
 var displayIssues = function(issues) {
     if (issues.length === 0) {
@@ -59,3 +77,5 @@ var displayWarning = function(repo) {
     // append to warning container
     limitWarningEl.appendChild(linkEl);
 };
+
+getRepoName();
